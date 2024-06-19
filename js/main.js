c2a9955422e73f8f282e92d894207b61caa824a8
Admilson Gonçalves Júnior <admilson.goncalves@gmail.com>
2024-06-19 20:47:48 -0300

    Menu responsivo terminado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let mainMenu = document.querySelector(' .main-menu');
+let menuIcon = document.querySelector('.menu-icon');
+let closeIcon = document.querySelector('.close-icon');
 
 function openMenu() {
   mainMenu.classList.add('open');
@@ -8,14 +10,20 @@ function closeMenu() {
   mainMenu.classList.remove('open');
 }
 
+menuIcon.addEventListener('click', openMenu);
+closeIcon.addEventListener('click', closeMenu);
+
+
 function checkScreenWidth() {
   if (window.matchMedia('(max-width: 768px)').matches) {
     mainMenu.classList.remove('open');
   }
 }
 
+// Add an event listener to check the screen width on resize
+window.addEventListener('resize', checkScreenWidth);
+
 // Run the function on initial load
 checkScreenWidth();
 
-// Add an event listener to check the screen width on resize
-window.addEventListener('resize', checkScreenWidth);
+
